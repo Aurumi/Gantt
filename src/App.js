ae687288e8c1e865e49a1081583c7945e5b773de
Aurumi <fargysst@mail.ru>
2021-07-23 10:53:08 +0300

    feat: gantt

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Gantt from './Gantt';
+
+const App = () => {
+    return <Gantt />;
+};
+
+export default App;
---
+++ src/Gantt.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import ReactGantt from 'gantt-for-react';
+
+const Gantt = () => {
+    const [viewMode, setViewMode] = useState('Day');
+    const [tasks, setTasks] = useState();
+
+    const getTasks = () => {
+        let names = [
+            ['Redesign website', [0, 7]],
+            ['Write new content', [1, 4]],
+            ['Apply new styles', [3, 6]],
+            ['Review', [7, 7]],
+            ['Deploy', [8, 9]],
+            ['Go Live!', [10, 10]],
+        ];
+
+        let tasks = names.map(function (name, i) {
+            let today = new Date();
+            let start = new Date(
+                today.getFullYear(),
+                today.getMonth(),
+                today.getDate()
+            );
+            let end = new Date(
+                today.getFullYear(),
+                today.getMonth(),
+                today.getDate()
+            );
+            start.setDate(today.getDate() + name[1][0]);
+            end.setDate(today.getDate() + name[1][1]);
+            return {
+                start: start,
+                end: end,
+                name: name[0],
+                id: 'Task ' + i,
+                progress: parseInt(Math.random() * 100, 10),
+            };
+        });
+
+        tasks[1].dependencies = 'Task 0';
+        tasks[2].dependencies = 'Task 1, Task 0';
+        tasks[3].dependencies = 'Task 2';
+        tasks[5].dependencies = 'Task 4';
+
+        tasks[0].custom_class = 'bar-milestone';
+        tasks[0].progress = 60;
+
+        tasks.slice(0, parseInt(Math.random() * 4 + 1));
+        setTasks(tasks);
+    };
+
+    const customPopupHtml = (task) => {
+        const end_date = task._end.format('MMM D');
+        return `
+          <div class="details-container">
+            <h5>${task.name}</h5>
+            <p>Expected to finish by ${end_date}</p>
+            <p>${task.progress}% completed!</p>
+          </div>
+        `;
+    };
+
+    useEffect(() => {
+        setInterval(() => {
+            setViewMode(
+                ['Quarter Day', 'Half Day', 'Day', 'Week', 'Month'][
+                    parseInt(Math.random() * 5 + 1) - 1
+                ]
+            );
+            getTasks();
+        }, 5000);
+    }, []);
+
+    if (!tasks) {
+        return 'Загрузка...';
+    }
+    return <ReactGantt tasks={tasks} viewMode={viewMode} />;
+};
+
+export default Gantt;
